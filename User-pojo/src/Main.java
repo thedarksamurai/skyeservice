bf94af1458dae07792e6bf68e2a97e84956708aa
thedarksamurai <halokiller36@gmail.com>
2022-12-20 12:55:29 -0700

    new user service added

--- User-pojo/src/Main.java
+++ User-pojo/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        System.out.println("Hello operator, ordis is happy to see you.");
+        userService UserService = new userService();
+        User ivaraUser = UserService.createUser("ivaraPrime@warframe.com", "Prime9020");
+        System.out.println(ivaraUser);
     }
 }
--- User-pojo/src/userService.java
+++ User-pojo/src/userService.java
@@ -1,48 +1,12 @@
 public class userService {
-    private String username;
-    private String password;
-    private String firstName;
-    private String lasName;
-    private int unsuccessfulLoginAttempts;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLasName() {
-        return lasName;
-    }
-
-    public void setLasName(String lasName) {
-        this.lasName = lasName;
-    }
-
-    public int getUnsuccessfulLoginAttempts() {
-        return unsuccessfulLoginAttempts;
-    }
-
-    public void setUnsuccessfulLoginAttempts(int unsuccessfulLoginAttempts) {
-        this.unsuccessfulLoginAttempts = unsuccessfulLoginAttempts;
+    public User createUser (String username, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setFirstName("Ivara");
+        user.setLasName("Prime");
+        return user;
     }
 }
 
